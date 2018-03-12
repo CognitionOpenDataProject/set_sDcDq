5560aeb47addc6905656ef73a4bc5e021213d98a
Tom Hardwicke <hardwickete@gmail.com>
2018-03-12 10:05:40 -0700

    fixed shell script

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-Rscript -e "rmarkdown::render(input = 'analysis.rmd', output_dir = '../results', clean = TRUE)"
+Rscript -e "rmarkdown::render(input = 'finalReport.Rmd', output_dir = '../results', clean = TRUE)"
